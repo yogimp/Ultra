f81a9555fc4476b6e10bb5015e68393c667c9cdc
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-06 18:48:33 +0700

    Navmenu Mobile Responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,73 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { IconContext } from 'react-icons/lib'
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements'
+import { Button } from '../../globalStyles'
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks, NavBtnLink, NavItemBtn } from './Navbar.elements'
 
 const Navbar = () => {
 
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click)
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+        setButton(false);
+        } else {
+        setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
         <IconContext.Provider value={{ color: '#fff' }}>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo to="/">
-                        <NavIcon/>
-                        ULTRA
+                    <NavLogo to='/' onClick={closeMobileMenu}>
+                    <NavIcon />
+                    ULTRA
                     </NavLogo>
                     <MobileIcon onClick={handleClick}>
-                        {click ? <FaTimes/> : <FaBars/>}
+                    {click ? <FaTimes /> : <FaBars />}
                     </MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                    <NavItem>
+                        <NavLinks to='/' onClick={closeMobileMenu}>
+                        Home
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='/services' onClick={closeMobileMenu}>
+                        Services
+                        </NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='/products' onClick={closeMobileMenu}>
+                        Products
+                        </NavLinks>
+                    </NavItem>
+                    <NavItemBtn>
+                        {button ? (
+                        <NavBtnLink to='/sign-up'>
+                            <Button primary>SIGN UP</Button>
+                        </NavBtnLink>
+                        ) : (
+                        <NavBtnLink to='/sign-up'>
+                            <Button onClick={closeMobileMenu} fontBig primary>
+                            SIGN UP
+                            </Button>
+                        </NavBtnLink>
+                        )}
+                    </NavItemBtn>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </IconContext.Provider>
